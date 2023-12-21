2871a1070f01b5370815ff368cc1cef89e8c4eaa
SylviaifyX <ifeomasylviaokpara@gmail.com>
2023-12-21 12:46:52 +0100

    todo list

--- index.js
+++ index.js
@@ -7,29 +7,71 @@ let showAllList = document.getElementById("showListItems")
 let closePop = document.querySelector(".close-pop-up")
 let all = []
 
-
-
 //calling the submit from the form when user input and click or listening for a submit event//
 galleryForm.addEventListener("submit", (e) =>{
     e.preventDefault()
-    formValidation()
-    showAll()
-    setTimeout(popList, 1000)  
-    galleryForm.reset();
+    if(validate(inputField.value.trim())){
+	    //appending the template to the  gallerylist  which is the ul//
+		gallery.innerHTML += generateListItem(inputField.value.trim())
+    	popList(inputField.value.trim())  
+    	galleryForm.reset();
+		}
 });
+
+//delete d todo//
+gallery.addEventListener("click", (e) =>{
+    //condition to check if the delete icon was clicked//
+ const value = e.target.parentElement.parentElement.previousElementSibling.innerHTML;
+    if(e.target.classList.contains('delete')){
+			popList(value,true)
+    }
+		if(e.target.classList.contains('edit')){
+			inputField.value = value;
+    }
+      e.target.parentElement.parentElement.parentElement.remove()
+})
+
+//closing the pop list//
+closePop.addEventListener("click", closePopup);
+
+switchIcon.addEventListener("click", toogleMode)
+
+//pop list//
+function popList(text, isDelete){
+	const para = showAllList.querySelector('p')
+	const header = showAllList.querySelector('h4')
+	if(text)  para.textContent = text 
+ 	showAllList.style.display = "block"
+	if(isDelete){
+		header.innerHTML = "Item&nbsp;Deleted"
+	} else {
+		header.innerHTML = "New&nbsp;Item"
+	}
+	setTimeout( closePopup, 5000)
+}
+
+function closePopup(){
+    showAllList.style.display = "none"
+}
+
+//to change theme from dark to white mood//
+function toogleMode(){
+    switchIcon.classList.toggle("bx-sun")
+    document.body.classList.toggle("dark-theme")
+}
+
 //form validation//
-let formValidation = () =>{
-    let input_field = inputField.value.trim()  
-    if(input_field.length){
-        generateList(input_field);
-        // console.log(generateList)
+function validate(value){
+    if(value.length > 0){
        message.innerHTML =""
-    }if(!input_field){
+			return true;
+    } else {
         message.innerHTML = "input can't be empty"
+			return false;
     }
 }
 //getting the value the user input//
-const generateList = (input_field) =>{
+function generateListItem(input_field){
    const template =
     `<li class="box">
         <h1>${input_field}<h1>
@@ -38,45 +80,5 @@ const generateList = (input_field) =>{
         <i class='bx bx-edit edit'"></i>
         </div>                    
    </li>`
-
-    //appending the template to the  gallerylist  which is the ul//
-   gallery.innerHTML += template
-   console.log(gallery)
+	 return template;
 }
-
-//delete d todo//
-gallery.addEventListener("click", (e) =>{
-    //condition to check if the delete icon was clicked//
-    // console.log(e)
-    if(e.target.classList.contains('delete')){
-        //navigating to the parentElement//
-        e.target.parentElement.parentElement.parentElement.remove()
-    }if(e.target.classList.contains('edit')){//edit to list//
-        inputField.value= e.target.parentElement.parentElement.previousElementSibling.innerHTML
-        e.target.parentElement.parentElement.parentElement.remove()
-    }
-    // }   console.log(inputField)
-})
-
-//to change theme from dark to white mood//
-const ToogleMode = () =>{
-    switchIcon.classList.toggle("bx-sun")
-    document.body.classList.toggle("dark-theme")
-}
-switchIcon.addEventListener("click", ToogleMode)
-
-let showAll = () =>{
-    all.push =(inputField.value)
-    all 
-    console.log(all)
-
-}
-
-//pop list//
-let popList = () =>{
- showAllList.style.display = "block"
-}
-//closing the pop list//
-closePop.addEventListener("click", () => {
-    showAllList.style.display = "none"
-});
